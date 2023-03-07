5b3bdf34678a720982cad53c74d666f273728564
Denis Davydov <dvd9@yandex.ru>
2023-03-07 23:36:46 +0300

    Added gitignore, checkstyle, pom files

---
+++ src/main/java/ru/aston/astondevtraineesteam24/AstonDevTraineeSteam24Application.java
@@ -0,0 +1,16 @@
+/**
+package ru.aston.astondevtraineesteam24;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AstonDevTraineeSteam24Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AstonDevTraineeSteam24Application.class, args);
+    }
+
+}
+*/
+
